e30c69c7156cc18c60b899685dd80cb36a7da8c5
John Pham <newbhope8@gmail.com>
2025-10-28 20:31:37 +0900

    cpp time

---
+++ Source/Yaminabe2025/CameraRigUtils.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+// FVector UCameraRigUtils::GetRailVelocity(ACameraRig_Rail* Rail, float Position, float Delta)
+// {
+// 	Rail->GetVelocity();
+// }
---
+++ Source/Yaminabe2025/CameraRigUtils.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CameraRigUtils.generated.h"
+
+class ACineCameraRigRail;
+/**
+ * 
+ */
+UCLASS()
+class YAMINABE2025_API UCameraRigUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	// UFUNCTION(BlueprintCallable, Category = "CameraRig")
+	// static FVector GetRailVelocity(ACameraRig_Rail* Rail, float Position, float Delta);
+
+};
